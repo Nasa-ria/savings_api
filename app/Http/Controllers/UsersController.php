611a6e162799ef088b902307b904f22442f967f2
ria-hue <mumuninasaria@gmail.com>
2023-02-09 17:22:45 +0000

    commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -143,30 +143,39 @@ class UsersController extends Controller
 
       
 
-    public function changePassword(Request $request , $id)
+    public function changePassword(Request $request  )
 {     
-        $user=User::find($id);
-        if($user){
+        $user= auth('api')->user();
+        $password = $user->password;
+        // if($user){
+  
             $request->validate([
                 'old_password' => 'required',
-                'new_password' => 'required|confirmed',
+                'new_password' => 'required',
+                'confirm_password' => 'required'
             ]);
-            // $user = User::where('' ,'=' ,$id)->get(); 
-            if(!Hash::check($request->old_password, auth()->user()->password)){
-                return "error Old Password Doesn't match!";
+              if(Hash::check($request->old_password, $password)){
+                if($request->old_password == $request->confrim_password){
+                #Update the new Password
+                // User::whereId(auth()->user()->id)->update([
+                    $user->update([
+                    'password' => Hash::make($request->new_password)
+                ]);
+                 }
+                 return "new password mismatch";
             }
-    
-    
-            #Update the new Password
-            User::whereId(auth()->user()->id)->update([
-                'password' => Hash::make($request->new_password)
-            ]);
-            return "password updated";
+             return " error Old Password Doesn't match!";
+        
+            // #Update the new Password
+            // User::whereId(auth()->user()->id)->update([
+            //     'password' => Hash::make($request->new_password)
+            // ]);
+            // return "password updated";
 
 
 
 
-        }
+        // }
         // # Validation
         // $request->validate([
         //     'old_password' => 'required',  
