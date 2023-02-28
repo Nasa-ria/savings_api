935763b6e146591aee19637538b3f12e790f19fd
ria-hue <mumuninasaria@gmail.com>
2023-02-28 16:59:49 +0000

    commit

--- tests/Unit/AdminTest.php
+++ tests/Unit/AdminTest.php
@@ -2,17 +2,31 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class AdminTest extends TestCase
 {
+    use DatabaseMigrations;
+    use WithoutMiddleware;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_to_fetch_all_users()
     {
+        $this->withoutExceptionHandling();
+
+        $response = $this->get('api/users');
+        $response->assertStatus(200);
         $this->assertTrue(true);
+    }
+
+
+    public function test_to_fetch_all_user_transactions(){
+        
     }
 }
